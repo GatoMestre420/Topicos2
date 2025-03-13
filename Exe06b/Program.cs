a2e559f9597d4d760e62e066c8faf54819b0a6ee
LUAN CURT <luan.curt@cs.up.edu.br>
2025-03-13 10:44:10 -0300

    exe06b

--- Exe06b/Program.cs
+++ Exe06b/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int[] vetor = new int[10];
+
+//GERAR UM VALOR RANDOMICO 
+Random random = new Random();
+//Console.WriteLine(random.Next(100));
+
+
+//PREENCHER O VETOR COM VALORES ALEATÓRIOS ENTRE 1 E 100   cle
+for(int i = 0; i < vetor.Length; i++){
+    vetor[i] = random.Next(100);
+}
+
+
+ Console.WriteLine("Vetor antes da ordenação:");
+//IMPRIMIR OS VALORES ALEATÓRIOS
+for(int i = 0; i < vetor.Length; i++){
+    Console.Write(vetor[i] + " ");
+}   
+
+
+    Array.Sort(vetor);
+
+
+
+Console.WriteLine("\n");
+Console.Write("Vetor depois da ordenação: \n");
+//EXIBIR OS VALORES ORDENADOS
+for(int i = 0; i < vetor.Length; i++){
+    Console.Write(vetor[i] + " ");
+}   
+
+
+
